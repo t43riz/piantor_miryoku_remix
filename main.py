367d7ecceb7878c2b5e18f28d61c48296776f9d9
t43riz <t43riz@users.noreply.github.com>
2023-10-30 16:48:38 -0600

    Update main.py

--- main.py
+++ main.py
@@ -28,12 +28,13 @@ data_pin2 = board.GP0 if split_side == SplitSide.LEFT else board.GP1
 split = Split(
     split_side=split_side,
     split_type=SplitType.UART,
-    split_flip=False,
+    split_flip=False, 
     data_pin=data_pin,
     data_pin2=data_pin2
 )
 keyboard.modules.append(split)
 MOMENTARY = KC.MO(1)
+MOMENTARY2 = KC.MO(2)
 
 keyboard.keymap = [
 # BASE
@@ -41,7 +42,7 @@ keyboard.keymap = [
     KC.TAB,  KC.Q,  KC.W, KC.E, KC.R,  KC.T,      KC.Y,  KC.U,  KC.I, KC.O, KC.P,  KC.BSPC,
     KC.LSHIFT, KC.A, KC.S, KC.D, KC.F,  KC.G,     KC.H, KC.J, KC.K, KC.L, KC.SCOLON,  KC.QUOTE,
     KC.LCTRL, KC.Z, KC.X,  KC.C,  KC.V, KC.B,     KC.N, KC.M, KC.COMM,  KC.DOT,  KC.SLASH, KC.ENT,
-                    KC.LCMD , KC.SPACE, MOMENTARY,    KC.RALT, KC.SPACE, KC.ENT,
+                    KC.LCMD , KC.SPACE, MOMENTARY,    KC.RALT, KC.SPACE, MOMENTARY2,
 
 ],
 
@@ -52,11 +53,18 @@ keyboard.keymap = [
     KC.LCMD, KC.Z, KC.X,  KC.C,  KC.V, KC.B,                 KC.N, KC.M, KC.LEFT,  KC.DOWN,  KC.SLASH, KC.ENT,
                     KC.LCMD , KC.SPACE, KC.NO,             KC.RALT, KC.SPACE, KC.SLASH,
 ],
+# t2
+[
+    KC.TAB, KC.N1, KC.N2, KC.N3, KC.N4, KC.N5,     KC.MB_LMB, KC.MS_UP, KC.MB_RMB, KC.N9, KC.N0, KC.MINUS,
+    KC.LSHIFT, KC.A, KC.S, KC.D, KC.F,  KC.G,                 KC.MS_LEFT, KC.MS_DOWN, KC.MS_RIGHT, KC.UP, KC.RIGHT,  KC.BSPC,
+    KC.LCMD, KC.Z, KC.X,  KC.C,  KC.V, KC.B,                 KC.N, KC.M, KC.LEFT,  KC.DOWN,  KC.SLASH, KC.ENT,
+                    KC.LCMD , KC.SPACE, KC.NO,             KC.RALT, KC.SPACE, KC.SLASH,
+],
 
 ]
 
 layer_names_list = [
-"Base", "Tap",
+"Base", "Tap", "t2",
 ]
 
 if __name__ == '__main__':
